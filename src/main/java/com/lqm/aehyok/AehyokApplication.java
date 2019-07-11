79b470072e14440dff9367330c410e21def59bdb
刘启明 <455043818@qq.com>
2019-07-11 09:40:34 +0800

    初始化项目

---
+++ src/main/java/com/lqm/aehyok/AehyokApplication.java
@@ -0,0 +1,13 @@
+package com.lqm.aehyok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AehyokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AehyokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lqm/aehyok/AehyokApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lqm.aehyok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AehyokApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
